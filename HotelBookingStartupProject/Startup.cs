8f5d3c42ad717766773039b3593188a1d6d13a76
Steffen Dam <sdam@incom-it.dk>
2017-10-01 22:41:16 +0200

    Fixed issue with missing types

--- HotelBookingStartupProject/Startup.cs
+++ HotelBookingStartupProject/Startup.cs
@@ -1,4 +1,6 @@
-﻿using HotelBooking.Web.BusinessLogic;
+﻿using HotelBooking.Core.Interfaces;
+using HotelBooking.Domain;
+using HotelBooking.Web.BusinessLogic;
 using HotelBooking.Web.Data;
 using HotelBooking.Web.Data.Repositories;
 using HotelBooking.Web.Managers;
@@ -19,6 +21,23 @@ namespace HotelBooking.Web
         }
 
         public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<HotelBookingContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("HotelBookingContext")));
+
+            services.AddDbContext<HotelBookingContext>(opt => opt.UseInMemoryDatabase("HotelBookingDb"));
+            services.AddScoped<IRepository<Room>, RoomRepository>();
+            services.AddScoped<IRepository<Customer>, CustomerRepository>();
+            services.AddScoped<IRepository<Booking>, BookingRepository>();
+            services.AddScoped<IBookingManager, BookingManger>();
+            services.AddScoped<ICustomerManager, CustomerManager>();
+
+            services.AddMvc();
+
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
