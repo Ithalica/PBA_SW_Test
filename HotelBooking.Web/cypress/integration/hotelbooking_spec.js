00eab186ac070945b020691fb006de6d466d53a7
Daniel Bloch Christensen <daniel@incom-it.dk>
2017-11-03 15:48:15 +0100

    Update

--- HotelBooking.Web/cypress/integration/hotelbooking_spec.js
+++ HotelBooking.Web/cypress/integration/hotelbooking_spec.js
@@ -14,12 +14,13 @@
 describe("Create new booking", function () {
     it("Go to booking page", function () {
         cy.contains("Create New").click();
-    });
-
-    it("Create booking", function () {
         cy.get("#StartDate").type("2018-01-01");
         cy.get("#EndDate").type("2018-01-31");
         cy.get("select").select("2");
         cy.get("form").submit();
     });
+
+    it("Is on start page", function () {
+        cy.contains("Create New");
+    })
 });
